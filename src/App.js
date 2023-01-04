bc0bdae4a4e45991a8e76399111a0764eeeaf48e
Hakan AKSOY <aksoyhakan1990gmail.com>
2023-01-04 15:50:01 +0300

    Görevler yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Bolum from "./Bolum";
 
 function App() {
+  let todayDate = new Date();
+  let day = todayDate.getDate();
+  let month = todayDate.getMonth() + 1;
+  let year = todayDate.getFullYear();
+  let dateObj = {
+    year: year,
+    month: month,
+    day: day,
+  };
+
+  const [info, setInfo] = useState({});
+  const [date, setDate] = useState(dateObj);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date.year}-${date.month}-${date.day}`
+      )
+      .then((response) => {
+        setInfo(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  function pickDate(callbackDate) {
+    let arrayDate = callbackDate.split("-");
+    let objDate = {
+      year: arrayDate[0],
+      month: arrayDate[1],
+      day: arrayDate[2],
+    };
+    setDate(objDate);
+  }
+  console.log(date);
+  console.log(
+    `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date.year}-${date.month}-${date.day}`
+  );
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <label for="dateSelection">Select Date: </label>
+      <input
+        type="date"
+        id="dateSelection"
+        name="dateSelectionNasa"
+        onChange={(event) => {
+          pickDate(event.target.value);
+        }}
+      ></input>
+      <Bolum gidenData={info} />
     </div>
   );
 }
---
+++ src/Bolum.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Bolum = (props) => {
+  const { gidenData } = props;
+  console.log("Bölüm çalıştı");
+  console.log(gidenData);
+
+  if (!gidenData.copyright) {
+    return (
+      <h3 style={{ margin: "0 auto", padding: "30vh 0", fontSize: "10rem" }}>
+        Yükleniyor...
+      </h3>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          margin: "3rem",
+          paddingLeft: "2rem",
+          border: "0.5rem solid red",
+        }}
+      >
+        <p style={{ fontSize: "3rem", fontWeight: "bold" }}>
+          {gidenData.title}
+        </p>
+        <div>
+          <img src={gidenData.url}></img>
+        </div>
+        <p>Copyright: {gidenData.copyright}</p>
+        <p style={{ textAlign: "left", textDecoration: "underline" }}>
+          Data Taken from Nasa {gidenData.date}
+        </p>
+        <p>{gidenData.explanation}</p>
+      </div>
+    );
+  }
+};
+
+export default Bolum;
